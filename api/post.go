3a881d775a9882dbaae29676700bd5a892f804be
Aaron Crowder <aaron@govalo.com>
2022-05-29 18:08:30 -0600

    let's see

--- api/post.go
+++ api/post.go
@@ -1,10 +1,22 @@
 package handler
 
 import (
-	"fmt"
+	"context"
 	"net/http"
+
+	"github.com/google/go-github/v45/github"
+	"golang.org/x/oauth2"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "... your access token ..."},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/crowdersoup/indieweb-proof-of-concept
 
 go 1.18
+
+require github.com/google/go-github/v45 v45.0.0 // indirect
